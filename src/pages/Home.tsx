cbf2d522943aa30a4d3ec84fb8970cfc678f6d69
bloodymoon <1270046784@qq.com>
2024-07-29 23:33:42 +0800

    feat: 添加深色模式
    Signed-off-by: bloodymoon <1270046784@qq.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const { Header, Content, Footer } = Layout
 export default function Home() {
     return (
         <Layout className="min-h-screen">
-            <Header className="flex h-16 flex-row items-center bg-white p-5">
+            <Header className="flex h-16 flex-row items-center bg-slate-50 p-5 text-black">
                 <TopNavigator />
             </Header>
-            <Content className="flex grow flex-col p-4">
+            <Content className="flex grow flex-col bg-slate-50 p-4 text-black">
                 <Outlet />
             </Content>
-            <Footer className="flex h-10 items-center justify-center bg-white text-center">
+            <Footer className="flex h-10 items-center justify-center bg-slate-50 text-center text-black">
                 Powered by&nbsp;
                 <Link className="text-blue-500" target="_blank" to="https://github.com/FriendshipMagic">
                     FriendshipMagic

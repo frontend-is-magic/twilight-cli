7017334003fe013b45c2bfad5d279534f249a684
bloodymoon <1270046784@qq.com>
2024-07-30 23:57:34 +0800

    feat: 创建资源页
    Signed-off-by: bloodymoon <1270046784@qq.com>

--- src/pages/Resource.tsx
+++ src/pages/Resource.tsx
@@ -1,9 +1,16 @@
 import ToolCard from '../components/ToolCard.tsx'
+import { devTools } from '../utils/constant.tsx'
 
 export default function Resource() {
     return (
-        <div className="flex h-full w-full grow flex-col items-center justify-center">
-            <ToolCard />
+        <div className="flex h-full w-full grow flex-col items-center justify-center space-y-20">
+            {new Array(2).fill(0).map((_, rowIndex) => (
+                <div key={rowIndex} className="flex flex-row items-center justify-center space-x-40">
+                    {devTools.slice(rowIndex * 3, rowIndex * 3 + 3).map((tool, index) => (
+                        <ToolCard key={index} {...tool} />
+                    ))}
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/utils/constant.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+import { ReactRouterIcon, ZustandIcon } from '../components/icons.tsx'
+
+export interface DevTool {
+    name: string
+    icon: React.ReactNode
+    link: string
+}
+
+export const devTools: DevTool[] = [
+    {
+        name: 'React',
+        icon: <Icon icon="devicon:react" className="text-8xl" />,
+        link: 'https://react.dev/',
+    },
+    {
+        name: 'Vite',
+        icon: <Icon icon="devicon:vitejs" className="text-8xl" />,
+        link: 'https://vitejs.dev/',
+    },
+    {
+        name: 'React Router',
+        icon: <ReactRouterIcon />,
+        link: 'https://reactrouter.com/en/main',
+    },
+    {
+        name: 'Zustand',
+        icon: <ZustandIcon />,
+        link: 'https://zustand-demo.pmnd.rs/',
+    },
+    {
+        name: 'Ant Design',
+        icon: <Icon icon="devicon:antdesign" className="text-8xl" />,
+        link: 'https://ant.design/index-cn',
+    },
+    {
+        name: 'Tailwind CSS',
+        icon: <Icon icon="devicon:tailwindcss" className="text-8xl" />,
+        link: 'https://tailwindcss.com',
+    },
+]
